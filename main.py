04b0fe20ff9e3563c769ef205a8894b8a9e5d3bb
norkos <norbert.kosmal@gmail.com>
2022-10-10 15:57:24 +0200

    Improving reliability

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 import json
 import pika
 import os
+import uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+PORT = os.environ.get('PORT', '8080')
+params = pika.URLParameters(os.getenv('CLOUDAMQP_URL'))
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='main', durable=True)
+
+messages = []
 
 
-def main():
-    params = pika.URLParameters(os.getenv('CLOUDAMQP_URL'))
-    connection = pika.BlockingConnection(params)
-    channel = connection.channel()
-    channel.queue_declare(queue='main', durable=True)
-
-    def callback(ch, method, properties, body):
-        data = json.loads(body)
-        print(data)
-
-    channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
-    print('Started consuming')
-
-    channel.start_consuming()
+def callback(ch, method, properties, body):
+    data = json.loads(body)
+    messages.append(data)
+    print(data)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-if __name__ == '__main__':
-    main()
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=False)
+print('Started consuming')
+
+channel.start_consuming()
+
+
+@app.get("/")
+async def root():
+    return {"message": messages}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=int(PORT),
+        workers=2
+    )
+
+
+
+
+
